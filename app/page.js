588b876e58eef43556b4280f565bbb347682fbb2
Steevel <57296025+Steevel@users.noreply.github.com>
2023-08-12 10:11:20 +0530

    feat - added google authentication

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
-import Image from "next/image";
+"use client";
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session?.user) {
+      router.push("/login");
+    }
+  }, [session]);
+
   return (
     <div>
       <h2>Some random text</h2>
+      <button onClick={() => signOut()}>Sign Out</button>
     </div>
   );
 }
